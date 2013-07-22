29b2bc3b7fa3fa150c0511723464329ec8f6e321
huangdada <huangdada@huangdada-PC>
2013-07-23 03:31:31 +0900

    changed the interfaces names and moved them to plugman package CloudInterface has been changed a lot.
    Signed-off-by: huangdada <huangdada@huangdada-PC>

---
+++ src/vcluster/plugman/BatchInterface.java
@@ -0,0 +1,20 @@
+package vcluster.plugman;
+
+import java.io.File;
+
+/**
+ * BatchInterface interface,all the batch system plug-in should implements this interface.
+ *
+ */
+public interface BatchInterface {
+	//connect to batch system,conf is a file includes the connection informations.
+	public boolean ConnectTo(File conf);
+	public boolean check_pool();
+	public boolean check_q();
+	public int getTotalJob();
+	public int getIdleJob();
+	public int getRunningJob();
+	public int getHeldJob();
+	public String getInfo();
+
+}
---
+++ src/vcluster/plugman/CloudInterface.java
@@ -0,0 +1,24 @@
+package vcluster.plugman;
+
+import java.util.List;
+
+
+public interface CloudInterface {
+	
+	//connect to a cloud system,conf is a file includes the connection informations
+	public boolean RegisterCloud(List<String> configurations);
+	//create a VM 
+	public boolean createVM();
+	//list all the VMS
+	public boolean listVMs();
+	//destroy a VM
+	public boolean destroyVM(String id);
+	//restart a suspendVM
+	public boolean startVM(String id);
+	// suspend a VM
+	public boolean suspendVM(String id);
+	// return the information about a cloud plugin,like a user Manual
+	public String getInfo();
+	
+	
+}
