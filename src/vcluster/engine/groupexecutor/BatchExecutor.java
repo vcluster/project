b32a8b11d04410b3d134acfe8e130c209d1b0a56
huangdada <huangdada@huangdada-PC>
2013-07-23 03:27:58 +0900

    Create a class to perform the functions of batch system through invoking the batch plugin's functions. Signed-off-by: huangdada <huangdada@huangdada-PC>

---
+++ src/vcluster/engine/groupexecutor/BatchExecutor.java
@@ -0,0 +1,34 @@
+package vcluster.engine.groupexecutor;
+
+import java.io.File;
+
+import vcluster.plugman.PluginManager;
+
+public class BatchExecutor {
+
+	public static boolean ConnectTo(File conf){
+		return PluginManager.current_proxyExecutor.ConnectTo(conf);
+	}
+	public static boolean check_pool(){
+		return PluginManager.current_proxyExecutor.check_pool();
+	}
+	public static boolean check_q(){
+		return PluginManager.current_proxyExecutor.check_q();
+	}
+	public static int getTotalJob(){
+		return PluginManager.current_proxyExecutor.getTotalJob();
+	}
+	public static int getIdleJob(){
+		return PluginManager.current_proxyExecutor.getIdleJob();
+	}
+	public static int getRunningJob(){
+		return PluginManager.current_proxyExecutor.getRunningJob();
+	}
+	public static int getHeldJob(){
+		return PluginManager.current_proxyExecutor.getHeldJob();
+	}
+	public static String getInfo(){
+		return PluginManager.current_proxyExecutor.getInfo();
+	}
+
+}
