151ec327e29ef8c9f58b638a657d8f61d2cf9928
huangdada <huangdada@huangdada-PC>
2013-07-30 01:28:06 +0900

    added an initialization of vmman Signed-off-by: huangdada <huangdada@huangdada-PC>

--- src/vcluster/global/VCluster.java
+++ src/vcluster/global/VCluster.java
@@ -5,6 +5,7 @@ public class VCluster {
 
 	public static boolean init() {
 		Config.cloudMan = new vcluster.control.cloudman.CloudManager();
+		Config.vmMan = new vcluster.control.VMManager();
 		return true;
 	}
 }
