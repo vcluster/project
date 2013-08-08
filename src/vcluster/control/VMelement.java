d4b59f0e448ede587dbe70924638bdb879e306c3
huangdada <huangdada@huangdada-PC>
2013-08-08 11:13:33 +0900

    Created a new Class VMelement,whose instances represent a virtual machine.
    Signed-off-by: huangdada <huangdada@huangdada-PC>

---
+++ src/vcluster/control/VMelement.java
@@ -0,0 +1,82 @@
+package vcluster.control;
+
+public class VMelement {
+	private String id;
+	private String state;
+	private String name;
+	private String memory;
+	private String user;
+	private String hostname;
+	private String time;
+	private String ucpu;
+	private String group;
+	private String cloudName;
+	
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMemory() {
+		return memory;
+	}
+	public void setMemory(String memory) {
+		this.memory = memory;
+	}
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	public String getHostname() {
+		return hostname;
+	}
+	public void setHostname(String hostname) {
+		this.hostname = hostname;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public String getUcpu() {
+		return ucpu;
+	}
+	public void setUcpu(String ucpu) {
+		this.ucpu = ucpu;
+	}
+	public String getGroup() {
+		return group;
+	}
+	public void setGroup(String group) {
+		this.group = group;
+	}
+	public String getCloudName() {
+		return cloudName;
+	}
+	public void setCloudName(String cloudName) {
+		this.cloudName = cloudName;
+	}
+	
+	
+	
+	
+	
+
+}
